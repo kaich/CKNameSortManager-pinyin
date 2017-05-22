91ea52b66d56fb90ca0e7a367371d3cad4d9643c
kaich <chengkai1853@163.com>
2017-05-22 16:01:28 +0800

    for swift

--- Pod/Classes/CKNameSortManager.h
+++ Pod/Classes/CKNameSortManager.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 typedef id (^ DataSourceBlock)(NSInteger index);
-typedef NSUInteger (^ DataSourceCountBlock)();
+typedef NSInteger (^ DataSourceCountBlock)();
 typedef void(^DataSourceSortCompleteBlock)(NSArray * finalDataSource);
 typedef NSString* (^ DataSourceKeywordBlock)(id  name);
 typedef NSPredicate *(^DataSourceFilterBlock)();
